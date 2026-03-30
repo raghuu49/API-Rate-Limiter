3b2f024a9454ee4a7d2775597732001c1d074df8
raghuu49 <raghuraj0511@gmail.com>
2026-03-30 17:27:19 +0530

    v1 done, now moving to real world systems

---
+++ limiter/TokenBucketLimiter.js
@@ -0,0 +1,45 @@
+const maxCapacity=5
+const refillRate=2.5
+
+const reqMap=new Map()
+//userId->{tokens,last_refill time}
+
+function TokenBucket(request){
+    const userId=request.userId
+    const timestamp=request.timestamp
+
+    if(!reqMap.has(userId)){
+        reqMap.set(userId,{tokens:maxCapacity,lastRefillTime:timestamp})
+    }
+
+    let user=reqMap.get(userId)
+    let lastRefillTime=user.lastRefillTime
+    let tokens=user.tokens
+    let elapsedTime=timestamp-lastRefillTime
+    let tokensToBeAdded=elapsedTime*refillRate
+    
+
+    tokens=Math.min(tokens+tokensToBeAdded,maxCapacity)
+    lastRefillTime=timestamp
+    if(tokens>=1){
+        tokens-=1
+        console.log("Request Accepted")
+        reqMap.set(userId,{tokens,lastRefillTime})
+    }
+    else{
+        console.log("Request Rejected")
+    }
+    console.log(
+  `User ${userId} | Tokens: ${tokens.toFixed(2)}`
+)
+    
+}
+
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:1,timestamp:2})
+TokenBucket({userId:2,timestamp:1})
+TokenBucket({userId:2,timestamp:1})
